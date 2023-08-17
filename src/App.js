94131d3dd3b2f0be94b1aec70f30c0d3c222d672
Rouguiyatou Diallo <rouguiyatoudiallo700@gmail.com>
2023-08-17 17:05:58 +0000

    cloud

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { 
+  return (
+    <div className="App">
+<iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=Point%20E%20Dakar+(gomycode)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+
+    </div>
+  );
+}
+
+export default App;
